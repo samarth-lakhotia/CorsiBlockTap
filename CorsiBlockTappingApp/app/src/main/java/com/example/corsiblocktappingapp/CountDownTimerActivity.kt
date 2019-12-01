1704f00d709cf9fca65c6134ead7e09b20831347
Wenbai <cheng_w_b@vip.sina.com>
2019-12-01 17:34:21 -0500

    Count Down Timer Activity added

--- CorsiBlockTappingApp/app/src/main/java/com/example/corsiblocktappingapp/CountDownTimerActivity.kt
+++ CorsiBlockTappingApp/app/src/main/java/com/example/corsiblocktappingapp/CountDownTimerActivity.kt
@@ -10,9 +10,7 @@ import android.widget.TextView
 class CountDownTimerActivity : Activity() {
 
     private lateinit var mCountDownTimer:TextView
-    private lateinit var timer:MyCounter
-    private val REQUEST_CODE_1 = 101
-    private val REQUEST_CODE_2 = 102
+    private lateinit var timer:CrossCountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +18,7 @@ class CountDownTimerActivity : Activity() {
 
         mCountDownTimer = findViewById(R.id.textView_countdown) as TextView
 
-        timer = MyCounter(5000, 1000)
+        timer = CrossCountDownTimer(5000, 1000)
         timer.start()
         Handler().postDelayed({
             this.setResult(RESULT_OK,Intent(this, MainActivity::class.java))
@@ -31,7 +29,7 @@ class CountDownTimerActivity : Activity() {
 
 
 
-    inner class MyCounter(millisInFuture: Long, countDownInterval: Long) : CountDownTimer(millisInFuture, countDownInterval) {
+    inner class CrossCountDownTimer(millisInFuture: Long, countDownInterval: Long) : CountDownTimer(millisInFuture, countDownInterval) {
 
         override fun onFinish() {
 
