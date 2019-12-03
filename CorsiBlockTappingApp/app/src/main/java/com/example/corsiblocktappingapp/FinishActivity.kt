f2afdc14fde5d9eab3d690d5e9dcf3c4f1994205
aman <amank98@gmail.com>
2019-12-03 17:28:34 -0500

    my more temp changes

--- CorsiBlockTappingApp/app/src/main/java/com/example/corsiblocktappingapp/FinishActivity.kt
+++ CorsiBlockTappingApp/app/src/main/java/com/example/corsiblocktappingapp/FinishActivity.kt
@@ -50,7 +50,9 @@ class FinishActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
 
+        // receives the gameSession from the GameActivity
         val gameData = intent.getStringExtra("GameData")
+
         // writes to /storage/emulated/0/Android/data/com.example.corsiblocktappingapp/files/MyFileStorage because
         // getExternalStorageDirectory is now deprecated. This folder can be accessed via ES FileExplorer.
         val fileWriter = FileWriter(getExternalFilesDir("MyFileStorage").toString() + "/DataFromSession.csv")
@@ -71,6 +73,7 @@ class FinishActivity : AppCompatActivity() {
         playButton.text = "Try Again"
         firebaseButton.text = "Upload to Firebase"
 
+        // the button that triggers another game
         playButton.setOnClickListener {
             startActivity(Intent(this, GameActivity::class.java))
         }
@@ -79,16 +82,18 @@ class FinishActivity : AppCompatActivity() {
         // writing the data from each round to a csv file
         csvButton.setOnClickListener {
             try {
+                //write the header
                 fileWriter.append(csvHeader)
                 fileWriter.append('\n')
 
-
+                //write the gameData
                 fileWriter.append(gameData.toString())
 
                 Log.i("CSV", "Wrote to CSV successfully!")
                 Toast.makeText(this, "Wrote to CSV successfully", Toast.LENGTH_LONG)
                     .show()
             } catch (e: Exception) {
+                // do this when any Exception is caught
                 Log.i("CSV", "Did not write to CSV successfully!")
                 Toast.makeText(this, "Did not write to CSV successfully", Toast.LENGTH_LONG)
                     .show()
@@ -98,6 +103,7 @@ class FinishActivity : AppCompatActivity() {
                     fileWriter.flush()
                     fileWriter.close()
                 } catch (e: IOException) {
+                    // if unable to flush or close successfully, log the info
                     Log.i("CSV", "flushing/closing Error!")
                     Toast.makeText(this, "Closing Error", Toast.LENGTH_LONG)
                         .show()
@@ -106,18 +112,22 @@ class FinishActivity : AppCompatActivity() {
             }
         }
 
+        // getting the reference to the FirebaseDatabase
         dbReference = FirebaseDatabase.getInstance().getReference("usersData")
+
+        // when the user clicks on the upload button, upload the data to firebase
         firebaseButton.setOnClickListener {
             Toast.makeText(this, "Uploading data to Firebase", Toast.LENGTH_LONG)
                 .show()
 
-            Log.i("ASDF", gameData.toString())
+            Log.i("CSV", gameData.toString())
             dbReference.setValue(gameData)
         }
 
 
         // if WiFi is enabled, then make the firebase upload button enabled
         firebaseButton.isEnabled = (checkForNetwork(baseContext))
+
         // otherwise, show a toast telling the user to connect to WiFi
         if (!checkForNetwork(baseContext)) {
             Toast.makeText(this, "Please connect to WiFi in order to upload data to Firebase", Toast.LENGTH_LONG)
@@ -125,6 +135,7 @@ class FinishActivity : AppCompatActivity() {
         }
     }
 
+    // a functoin that checks for wifi connection
     private fun checkForNetwork(context: Context): Boolean {
         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         // if past API level 23, use the new method. Otherwise, use the deprecated method
@@ -138,28 +149,11 @@ class FinishActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
     override fun onBackPressed() {
         startActivity(Intent(this, MainActivity::class.java))
     }
     fun getJSON(){
         var jsobj=JSONObject()
         jsobj.put("Session", {})
-
-
     }
 }
